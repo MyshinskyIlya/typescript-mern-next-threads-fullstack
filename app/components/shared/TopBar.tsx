19e7b39cffbd65ee1bb60dcbdf35dcf2bd79a66d
MyshinskyIlya <myshinskiyyy@gmail.com>
2023-12-30 23:20:34 +0400

    Added a favicon

--- app/components/shared/TopBar.tsx
+++ app/components/shared/TopBar.tsx
@@ -4,13 +4,16 @@ import Link from "next/link";
 function TopBar() {
     return (
         <nav className="topbar">
-            <Link href={"/"} className="items-center gap-4">
+            <Link href={"/"} className="flex items-center gap-4">
                 <Image
                     src={"/logo.svg"}
                     alt="Logo Icon"
                     width={20}
                     height={20}
                 ></Image>
+                <p className="text-heading2-bold text-light-1 max-xs:hidden">
+                    Threads
+                </p>
             </Link>
         </nav>
     );
