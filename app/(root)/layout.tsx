cbb49ace2a0bb61b05cdf8e74b3380ed19e22a4d
MyshinskyIlya <myshinskiyyy@gmail.com>
2024-01-02 18:21:24 +0400

    st

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
 import "../globals.css";
 import { Inter } from "next/font/google";
-import TopBar from "../components/shared/TopBar";
-import LeftSideBar from "../components/shared/LeftSideBar";
-import RightSideBar from "../components/shared/RightSideBar";
-import BottomBar from "../components/shared/BottomBar";
+import TopBar from "../../components/shared/TopBar";
+import LeftSideBar from "../../components/shared/LeftSideBar";
+import RightSideBar from "../../components/shared/RightSideBar";
+import BottomBar from "../../components/shared/BottomBar";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { sidebarLinks } from "../../../constants/index";
+import { sidebarLinks } from "../../constants/index";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { SignOutButton, SignedIn } from "@clerk/nextjs";
