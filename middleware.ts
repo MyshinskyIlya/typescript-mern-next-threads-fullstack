349016445caaa0e5906c9989c4d9b3136967c9fe
MyshinskyIlya <myshinskiyyy@gmail.com>
2024-01-10 22:20:39 +0400

    Fix bug for uploading an image

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes: ["/", "/api/webhook/clerk", "/api/uploadthing"],
-    ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
