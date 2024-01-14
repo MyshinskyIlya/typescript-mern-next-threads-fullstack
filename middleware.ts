7ea70a6fb188dcce68f275b21d31fd4176cfb751
MyshinskyIlya <myshinskiyyy@gmail.com>
2024-01-14 11:54:44 +0400

    fx

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/api/webhook/clerk"],
-
-    ignoredRoutes: ["/api/webhook/clerk"],
+    publicRoutes: ["/", "/api/webhook/clerk", "/api/uploadthing"],
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
